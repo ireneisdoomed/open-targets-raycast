39c003489dddd44ce22733c3390d6df4b7dd10ee
Irene López <irene.lopezs@protonmail.com>
2023-01-29 20:27:15 +0000

    feat: more complex metadata + copy to clipboard

--- src/inspect_schema.tsx
+++ src/inspect_schema.tsx
@@ -1,8 +1,6 @@
-import { List } from '@raycast/api'
+import { ActionPanel, Action, List, Icon } from '@raycast/api'
 import { Dataset, otDatasets } from "./ot_datasets";
 import { useCachedPromise } from '@raycast/utils';
-
-console.log(otDatasets[0].schema_fields)
 
 export default function Command() {
     const { data, isLoading } = useCachedPromise(() => new Promise<Dataset[]>((resolve) => resolve(otDatasets)));
@@ -15,9 +13,31 @@ export default function Command() {
                             <List.Item
                                 key={dataset.name}
                                 title={dataset.name}
-                                icon="ot-icon.png"
+                                icon={Icon.Tag}
+                                actions={
+                                    <ActionPanel>
+                                        <Action.CopyToClipboard title="Copy location in GCS" content={dataset.location} />
+                                    </ActionPanel>
+                                }
                                 detail={
-                                    <List.Item.Detail markdown="Prueba de detalle" />
+                                    <List.Item.Detail 
+                                        metadata={
+                                            <List.Item.Detail.Metadata>
+                                                <List.Item.Detail.Metadata.Label title="Schema" />
+                                                <List.Item.Detail.Metadata.Separator />
+                                                {dataset.schema_fields.map((field) => {
+                                                    return (
+                                                        <List.Item.Detail.Metadata.Label
+                                                            key={field}
+                                                            title={field.name}
+                                                            icon={field.nullable === true ? Icon.Checkmark : Icon.XMarkCircleFilled}
+                                                            text={field.type}
+                                                        />
+                                                    );
+                                                })}
+                                            </List.Item.Detail.Metadata>
+                                        }
+                                    />
                                 }
                             />
                     );
--- src/ot_datasets.tsx
+++ src/ot_datasets.tsx
@@ -1,15 +1,52 @@
 
+const platform_bucket = "gs://open-targets-pre-data-releases/22.11/output/etl/parquet";
+const genetics_bucket = "gs://genetics-portal-dev-data/22.09.0/outputs";
+const genetics_dev_bucket = "gs://genetics_etl_python_playground/XX.XX/output/python_etl/parquet";
+
 export interface Dataset {
     name: string;
     type: string;
+    location: string;
     schema_fields: Array<any>;
 }
 
 export const study_index = [
     {'name': 'studyId', 'type': 'string', 'nullable': false},
-    {'name': 'projectId', 'type': 'string', 'nullable': false}
+    {'name': 'projectId', 'type': 'string', 'nullable': false},
+    {
+        "name": "discoverySamples",
+        "type": {
+            "type": "array",
+            "elementType": {
+            "type": "struct",
+            "fields": [
+                {
+                "name": "sampleSize",
+                "type": "string",
+                "nullable": true,
+                "metadata": {}
+                },
+                {
+                "name": "ancestry",
+                "type": "string",
+                "nullable": true,
+                "metadata": {}
+                }
+            ]
+            },
+            "containsNull": true
+        },
+        "nullable": true,
+        "metadata": {}
+    },
+    {
+      "name": "hasSumstats",
+      "type": "boolean",
+      "nullable": true,
+      "metadata": {}
+    }
 ]
 
 export const otDatasets : Dataset[] = [
-    { name: "Study index", type: "genetics", schema_fields: study_index },
+    { name: "Study index", type: "genetics", location: `${genetics_bucket}/lut/study-index`, schema_fields: study_index },
 ]
