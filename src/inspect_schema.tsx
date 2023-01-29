7f0909554a6f357ab9061bf4bca6996d1c461e60
Irene López <irene.lopezs@protonmail.com>
2023-01-29 22:23:43 +0000

    fix: correct field type if nested

--- src/inspect_schema.tsx
+++ src/inspect_schema.tsx
@@ -2,8 +2,6 @@ import { ActionPanel, Action, List, Icon } from '@raycast/api'
 import { Dataset, otDatasets } from './ot_datasets';
 import { useCachedPromise } from '@raycast/utils';
 
-// TODO: error in text when type != string
-
 export default function Command() {
     const { data, isLoading } = useCachedPromise(() => new Promise<Dataset[]>((resolve) => resolve(otDatasets)));
 
@@ -33,7 +31,7 @@ export default function Command() {
                                                             key={field}
                                                             title={field.name}
                                                             icon={field.nullable === true ? Icon.Checkmark : Icon.XMarkCircleFilled}
-                                                            text={field.type}
+                                                            text={typeof field.type === 'string' ? field.type : field.type.type}
                                                         />
                                                     );
                                                 })}
